745dd827b94ca40858bed98a975c10eb01586ba8
Nick Santolla <110840054+Santollan@users.noreply.github.com>
2025-01-30 12:44:29 +0000

    AF3-initial test

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Frankies
 Scalable, AI_Based Antibody Design 
+
+
+Snakemake Commands -
+
+## Run snakemake pipeline
+
+```snakemake --snakefile pipeline.txt```
---
+++ Scripts/3_diffusion/run_af3.sh
@@ -0,0 +1,11 @@
+docker run -it \
+    --volume $PWD/data/processed/3_diffusion/af_input:/root/af_input \
+    --volume $PWD/data/processed/3_diffusion/af_output :/root/af_output \
+    --volume /media/ssd2/Projects/Alphafold/Alphafold_weights :/root/models \
+    --volume /media/ssd2/Projects/Alphafold/Alphafold_DB :/root/public_databases \
+    --gpus all \
+    alphafold3 \
+    python run_alphafold.py \
+    --json_path=/root/af_input/fold_input.json \
+    --model_dir=/root/models \
+    --output_dir=/root/af_output

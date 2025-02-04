beba6760072a9f3fce669a44798397fc4aa44056
Colby Ford <colbytylerford@gmail.com>
2025-02-04 13:40:38 -0500

    Add HADDOCK3 test files

---
+++ Scripts/4_docking/haddock3/tests/run.sh
@@ -0,0 +1,6 @@
+
+## Run antigen preparation
+python ../prepare_antigen_inputs.py --input_pdb_path 2vir_chainC_antigen.pdb --output_pdb_path ./2vir_antigen_prepared.pdb --resn_offset 1000 --percentage 0.25
+
+## Run antibody preparation
+python ../prepare_antibody_inputs.py --input_pdb_path 2vir_chainsAB_antibody.pdb --output_pdb_path ./2vir_antibody_prepared.pdb --L_resn_offset 1000

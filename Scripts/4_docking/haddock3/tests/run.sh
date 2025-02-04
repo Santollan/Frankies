a6ef556e3e9e0880108572d96ef06cf522ff3ad0
Colby Ford <colbytylerford@gmail.com>
2025-02-04 13:56:44 -0500

    Update run.sh

--- Scripts/4_docking/haddock3/tests/run.sh
+++ Scripts/4_docking/haddock3/tests/run.sh
@@ -3,4 +3,4 @@
 python ../prepare_antigen_inputs.py --input_pdb_path 2vir_chainC_antigen.pdb --output_pdb_path ./2vir_antigen_prepared.pdb --resn_offset 1000 --percentage 0.25
 
 ## Run antibody preparation
-python ../prepare_antibody_inputs.py --input_pdb_path 2vir_chainsAB_antibody.pdb --output_pdb_path ./2vir_antibody_prepared.pdb --L_resn_offset 1000
+python ../prepare_antibody_inputs.py --input_pdb_path 2vir_chainsAB_antibody.pdb --output_pdb_path ./2vir_antibody_prepared.pdb --H_chain_id A --L_chain_id B --L_resn_offset 1000
